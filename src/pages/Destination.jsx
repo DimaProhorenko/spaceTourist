a2834d29fa7bb09265a52e14d691b7fbcb09fa11
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-11-30 00:22:16 +0300

    ADD destination jsx

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,8 +1,9 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import { Section, Content, Title } from '../components';
 import bgImage from '../assets/destination/background-destination-desktop.jpg';
 import destinations from '../fixtures/destinations';
+import { DESTINATION } from '../constants/routes';
 
 function Destination() {
 	const { name } = useParams();
@@ -19,6 +20,36 @@ function Destination() {
 							alt={item.name}
 							className="max-w-[300px] mx-auto block"
 						/>
+					</Content.Item>
+					<Content.Item>
+						<nav>
+							<ul className="flex gap-2 justify-center lg:justify-start">
+								{destinations.map((item) => (
+									<li key={item.id}>
+										<Link
+											to={`/${DESTINATION}/${item.name}`}
+										>
+											{item.name.toUpperCase()}
+										</Link>
+									</li>
+								))}
+							</ul>
+						</nav>
+						<h1 className="text-[56px] md:text-[80px] lg:text-[100px] uppercase">
+							{item.name}
+						</h1>
+						<p className="">{item.desc}</p>
+						<div className="h-px my-4 bg-slate-100 opacity-20"></div>
+						<div className="flex flex-col gap-4 md:flex-row md:justify-between">
+							<div>
+								<h3 className="uppercase">AVG. Distance</h3>
+								<h2 className="text-3xl">{item.avgDistance}</h2>
+							</div>
+							<div>
+								<h3 className="uppercase">EST. Travel Time</h3>
+								<h2 className="text-3xl">{item.travelTime}</h2>
+							</div>
+						</div>
 					</Content.Item>
 				</Content>
 			</div>
